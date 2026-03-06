8a4cd32c86a81b8c7e462f512a4f13ebe9154c8d
Divyansh Duggad <94543821+DDuggad@users.noreply.github.com>
2026-03-06 12:41:50 +0530

    Add non-preemptive scheduling in SJP program
    Implement non-preemptive scheduling algorithm for processes.

---
+++ OS/Lab2_06-03-2026/SJP (non-premptive).c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int n, time = 0, comp = 0, at[10], bt[10], ct[10], tat[10], wt[10], done[10] = {0};
+    float avg_wt = 0, avg_tat = 0;
+
+    printf("Enter no of processes: "); 
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        printf("Enter AT and BT for P%d: ", i + 1);
+        scanf("%d %d", &at[i], &bt[i]);
+    }
+
+    while (comp < n) {
+        int idx = -1, min_bt = 1e9;
+        for (int i = 0; i < n; i++) {
+            if (at[i] <= time && !done[i] && bt[i] < min_bt) {
+                min_bt = bt[i];
+                idx = i;
+            }
+        }
+        
+        if (idx != -1) {
+            time += bt[idx];
+            ct[idx] = time;
+            tat[idx] = ct[idx] - at[idx];
+            wt[idx] = tat[idx] - bt[idx];
+            avg_wt += wt[idx]; 
+            avg_tat += tat[idx];
+            done[idx] = 1; 
+            comp++;
+        } else {
+            time++;
+        }
+    }
+
+    printf("\nP\tAT\tBT\tCT\tTAT\tWT\n");
+    for (int i = 0; i < n; i++) printf("P%d\t%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], ct[i], tat[i], wt[i]);
+    printf("\nAvg WT: %.2f\nAvg TAT: %.2f\n", avg_wt / n, avg_tat / n);
+    return 0;
+}
